4ca56a3b38c1efeeda77f22ca35d1801d5aba7a0
pierrick <pierrickviret@gmail.com>
2025-07-09 22:18:36 +0200

    feat: implement dynamic project loading from JSON and update project list in HTML

--- animation.js
+++ animation.js
@@ -34,3 +34,85 @@
   links.href = `mailto:${email}`;
     };
 
+
+    document.addEventListener('DOMContentLoaded', function() {
+      const projetData = JSON.parse(projet.json);
+
+      const projectsContainer = document.getElementById('projectsPerso');
+      projectsContainer.innerHTML = ''; // Clear existing content
+
+       projetData.forEach(project => {
+        const projectCard = document.createElement('li');
+        projectCard.className = 'col-10 col-md-5 col-lg-3 p-0 m-2 bg-dark rounded card project-card';
+        
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body bg-dark text-white rounded';
+
+        const cardHeader = document.createElement('div');
+        cardHeader.className = 'card-header-content mb-2';
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = project.title;
+        const cardSubtitle = document.createElement('h6');
+        cardSubtitle.className = 'card-subtitle mb-1 text-secondary';
+        cardSubtitle.textContent = project.subtitle;
+        cardHeader.appendChild(cardTitle);
+        cardHeader.appendChild(cardSubtitle);
+        cardBody.appendChild(cardHeader);
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = project.text;
+        cardBody.appendChild(cardText);
+
+        const cardImage = document.createElement('div');
+        cardImage.className = 'project-image-container mb-3 mt-auto';
+        const image = document.createElement('img');
+        cardImage.src = `assets/photos/${project.photo}.webp`;
+        image.className = 'card-img';
+        image.alt = "image de ${project.photo}";
+        cardImage.appendChild(image);
+        cardBody.appendChild(cardImage);
+
+        const cardFooter = document.createElement('div');
+        cardFooter.className = 'd-flex justify-content-around align-items-center';
+        project.icones.forEach(icone => {
+          const iconElement = document.createElement('img');
+          iconElement.src = `assets/photos/icone/icone_${icone}.webp`;
+          iconElement.className = 'rounded techno_icone';
+          iconElement.alt = `Icone ${icone}`;
+          cardFooter.appendChild(iconElement);
+        })
+        cardBody.appendChild(cardFooter);
+
+        projectCard.appendChild(cardBody);
+
+        const cardOverlay = document.createElement('div');
+        cardOverlay.className = 'card-overlay';
+
+        const overlayTitle = document.createElement('h3');
+        overlayTitle.className = 'overlay-title';
+        overlayTitle.textContent = project.title;
+        cardOverlay.appendChild(overlayTitle);
+
+        const overlayText = document.createElement('p');
+        overlayText.className = 'overlay-text';
+        overlayText.textContent = project.text;
+        cardOverlay.appendChild(overlayText);
+
+        const overlayfeature = document.createElement('ul');
+        overlayfeature.className = 'overlay-features';
+        project.features.forEach(feature => {
+          const featureItem = document.createElement('li');
+          featureItem.textContent = feature;
+          overlayfeature.appendChild(featureItem);
+        });
+        cardOverlay.appendChild(overlayfeature);
+
+        projectCard.appendChild(cardOverlay);
+
+        projectsContainer.appendChild(projectCard);
+      }
+      );
+    });
