52d4f08b44485901db0807202ce350857440f664
pierrick <pierrickviret@gmail.com>
2025-10-15 22:14:34 +0200

    add(contactbutton) add lik to email in contact button

--- src/components/ui/contact-button.jsx
+++ src/components/ui/contact-button.jsx
@@ -1,9 +1,15 @@
 
 export function ContactBtn() {
+    const makeEmail = () => {
+        const coded = 'cGllcnJpY2t2aXJldEBnbWFpbC5jb20=' // base64
+        const email = atob(coded)
+        window.location.href = `mailto:${email}`
+    }
+
     return (
         <a class="fancy" href="#Contact">
             <span class="top-key"></span>
-            <span class="text">me contacter</span>
+            <a href="#" onClick={(e) => { e.preventDefault(); makeEmail(); }}><span class="text">me contacter</span></a>
             <span class="bottom-key-1"></span>
             <span class="bottom-key-2"></span>
         </a>
